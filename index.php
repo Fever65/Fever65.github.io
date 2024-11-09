db7fa8b83bfc86baaf662a987ea342e8fb50c702
Fever65 <sylvainmirtz@outlook.fr>
2024-11-09 21:34:45 +0100

    correction PHP 1

--- index.php
+++ index.php
@@ -1,13 +1,14 @@
+<?php include 'header.php'; ?>
+
 <!doctype html>
 <html lang="fr">
 <head>
   <meta charset="utf-8">
   <title>Mes projets</title>
   <link rel="stylesheet" href="style.css">
-  <script src="script.js"></script>
 </head>
 <body>
-<?php include 'header.php'; ?>
 <h1>Site en construction</h1>
+<p>Bienvenue sur mon site !</p>
 </body>
 </html>
